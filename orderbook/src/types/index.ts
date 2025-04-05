ad325c2e0a441f37196579ce87f45ed5fee87601
Satyam Gupta <satyam.bn6@gmail.com>
2025-04-05 21:20:41 +0530

    update

--- orderbook/src/types/index.ts
+++ orderbook/src/types/index.ts
@@ -11,3 +11,14 @@ export const loginschema=z.object({
     password:z.string()
 })
 
+
+export const OrderInputSchema = z.object({
+    baseAsset: z.string(),
+    quoteAsset: z.string(),
+    price: z.number(),
+    quantity: z.number(),
+    side: z.enum(['buy', 'sell']),
+    type: z.enum(['limit', 'market']),
+    kind: z.enum(['ioc']).optional(),
+  });
+
